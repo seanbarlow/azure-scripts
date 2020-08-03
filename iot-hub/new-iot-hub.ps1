a9321adf649c812a390d64c046ad376d22b00f56
Sean Barlow <sbarlow@barlowtech.com>
2020-08-02 20:54:49 -0500

    create iot hub

---
+++ iot-hub/new-iot-hub.ps1
@@ -0,0 +1,23 @@
+# Change the execution policy so unsigned scripts can be run.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# Log in to Azure Subscription
+Login-AzAccount
+
+# Resource Group Name Propt
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+
+# Location Prompt
+$location = Read-Host -Prompt "Enter the location (i.e. ""South Central US"")"
+
+# Create a new resource group setting the name and location
+$resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# IoT Hub Name Prompt
+$iotHubName = Read-Host -Prompt "Enter the IoT Hub name"
+
+# Create the IoT Hub
+$iotHub = New-AzIotHub -ResourceGroupName $resourceGroup.ResourceGroupName -Name $iotHubName -SkuName S1 -Units 1 -Location $location
+
+# Print the details of the IoT Hub to the console
+Write-Output $iotHub
